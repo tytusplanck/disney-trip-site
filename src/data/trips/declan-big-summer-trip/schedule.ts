5f550f0724697ef64f987bde72f6860f7869d95f
Tytus Planck <tytusplanck@gmail.com>
2026-04-21 16:34:09 -0400

    Plan tweaks

--- src/data/trips/declan-big-summer-trip/schedule.ts
+++ src/data/trips/declan-big-summer-trip/schedule.ts
@@ -7,21 +7,24 @@ export const declanBigSummerTripSchedule: TripScheduleEntry[] = [
     kinds: ['travel', 'park'],
     label: "Disney's Animal Kingdom / Epcot",
     parkLabel: "Disney's Animal Kingdom",
-    notes: 'Land at 9:31am at MCO. DAK day with the evening in EPCOT.',
+    notes:
+      'Flight land at 9:31am at MCO. We will knock out DAK for the trip and then enjoy Epcot at night time permitting.',
   },
   {
     date: '2026-07-08',
     kind: 'park',
     label: 'Magic Kingdom',
     parkLabel: 'Magic Kingdom',
-    notes: 'Fireworks',
+    notes:
+      'Plan to find a good spot for parade and fireworks. Prefer to get Beak and Barrel reservation.',
   },
   {
     date: '2026-07-09',
     kind: 'park',
     label: "Disney's Hollywood Studios",
     parkLabel: "Disney's Hollywood Studios",
-    notes: 'Fantasmic. Potential afternoon park hop to EPCOT.',
+    notes:
+      "Fantasmic at night. Optionally hop to Epcot if we are nervous about accomplishing everything. Prefer to get Oga's reservation.",
   },
   {
     date: '2026-07-10',
